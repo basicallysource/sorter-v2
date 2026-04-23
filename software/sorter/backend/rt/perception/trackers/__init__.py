3bbdf67d92f35c04b23d4ec5b4f5c473d149f0cd
Marc Neuhaus <marc@neuhaus.nrw>
2026-04-24 00:41:35 +0200

    Add Roboflow tracker adapters

--- software/sorter/backend/rt/perception/trackers/__init__.py
+++ software/sorter/backend/rt/perception/trackers/__init__.py
@@ -1,7 +1,8 @@
 """Tracker implementations. Import triggers registry self-registration."""
 
 from . import polar  # noqa: F401
+from . import roboflow  # noqa: F401
 from . import turntable_groundplane  # noqa: F401
 
 
-__all__ = ["polar", "turntable_groundplane"]
+__all__ = ["polar", "roboflow", "turntable_groundplane"]
